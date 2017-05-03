a7c03e157d853ab62482ff6383433ba49674e8cf
BAAMMM <BAAMMM@outlook.de>
2017-05-03 13:35:04 +0200

    Vorbereiten der Teststruktur der neuen Algorthmen

---
+++ src/test/algorithms/JUnitTestKruskal.java
@@ -0,0 +1,21 @@
+package test.algorithms;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+// TODO Kruskal test implemetation
+
+public class JUnitTestKruskal {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/algorithms/JUnitTestPrim.java
@@ -0,0 +1,20 @@
+package test.algorithms;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+// TODO Prim test implementation
+public class JUnitTestPrim {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
