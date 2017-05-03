08409ac0603202ed9f4078215907e31896b5140b
chris <chris@localhost>
2017-05-03 16:43:51 +0200

    Kommentiert

--- src/mvc/model/algorithmen/Algorithmus.java
+++ src/mvc/model/algorithmen/Algorithmus.java
@@ -5,54 +5,76 @@ import org.graphstream.graph.Node;
 
 import utility.Printer;
 
+/**
+ * Diese Klasse definiert den grundlegenden Aufbau eines Algorithmus. Ein
+ * Alogirthmus kann mit compute() ausgeführt werden. Dort wird er zunächst
+ * inizialisiert mit einem Graphen, einen Startknoten und einen Zielknoten.
+ * Anschließen wird die jeweilige Handlungsvorschrift, die die Einzelschritten
+ * des jeweiligen Algorithmus beinhaltet, ausgeführt. Dies ermöhlicht uns, dass
+ * die konkreten Klassen von Algorithmus sich nur um ihre jeweilige
+ * Handlungsvorschriftt kümmern müssen.
+ * 
+ * @author chris
+ *
+ */
 public abstract class Algorithmus {
-	
+
 	private Graph graph;
 	private Node source;
 	private Node target;
-		
-	// TODO command pattern für die Auswahl der Befehle?
-	
-	// TODO getResult() statt geShortestPath usw? Mit Prompt Ausgabe, was das Result gerade ist?
 
-	// TODO Graph mit übergeben, so dass es von jedem Algortihmus nur ein Objekt geben muss und die interne
-	// variable graph jedes mal geleert wird, vlt eine reset Algortihmus ? bzw im initilaize alle Variable zurück auf
-	// 
+	// TODO command pattern für die Auswahl der Befehle?
+
+	// TODO getResult() statt geShortestPath usw? Mit Prompt Ausgabe, was das
+	// Result gerade ist?
+
+	// TODO Graph mit übergeben, so dass es von jedem Algortihmus nur ein Objekt
+	// geben muss und die interne
+	// variable graph jedes mal geleert wird, vlt eine reset Algortihmus ? bzw
+	// im initilaize alle Variable zurück auf
+	//
 	/**
-	 * Diese Methode berechnet die interne Handlungsvorschrift aus und führt somit
-	 * den Algorithmus aus.
+	 * Diese Methode berechnet die interne Handlungsvorschrift aus und führt
+	 * somit den Algorithmus aus.
 	 * 
-	 * @param source Startknoten
-	 * @param target Zielknoten
+	 * @param source
+	 *            Startknoten
+	 * @param target
+	 *            Zielknoten
 	 * @return Graph auf den der Algorithmus angewandt wurde
 	 */
 	public Graph compute(Graph graph, Node source, Node target) {
-		Printer.prompt(this, "-------------------------------------"); // ohne this
+		Printer.prompt(this, "-------------------------------------"); // ohne
+																		// this
 		Printer.prompt(this, "compute algorithmus");
-		
+
 		this.initAlgorithmus(graph, source, target);
 
 		return this.procedure();
 	}
 
 	/**
-	 * Diese Mehtode stellt die interne Handlungsvorschrift eines jeden Algorithmus,
-	 * welche die Einzelschritten definiert, dar.
+	 * Diese Mehtode stellt die interne Handlungsvorschrift eines jeden
+	 * Algorithmus, welche die Einzelschritten definiert, dar.
 	 * 
-	 * @param source Startknoten
-	 * @param target Zielknoten
+	 * @param source
+	 *            Startknoten
+	 * @param target
+	 *            Zielknoten
 	 * @return Graph auf den der Algorithmus angewandt wurde
 	 */
 	protected abstract Graph procedure();
-	
+
 	/**
-	 * Initialisiert den Algorithmus auf einen Graphen, einen Startknoten sowie auf ein Zielknoten. Es ist
-	 * die einzige Methode um die sich eine konrekte Klasse Algorithmus kümmern muss.
+	 * Initialisiert den Algorithmus auf einen Graphen, einen Startknoten sowie
+	 * auf ein Zielknoten. Es ist die einzige Methode um die sich eine konrekte
+	 * Klasse Algorithmus kümmern muss.
+	 * 
 	 * @param graph
 	 * @param source
 	 * @param target
 	 */
-	private void initAlgorithmus(Graph graph, Node source, Node target){
+	private void initAlgorithmus(Graph graph, Node source, Node target) {
 		Printer.promptTestOut(this, "initialize");
 		Printer.promptTestOut(this, "set source: " + source.toString());
 		Printer.promptTestOut(this, "set target: " + target.toString());
@@ -72,8 +94,5 @@ public abstract class Algorithmus {
 	protected Node getTarget() {
 		return target;
 	}
-	
-	
-
 
 }
